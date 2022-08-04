e2942d14f5b57836b7345c5bdbb7758a8b475ec5
forsilence <3312034934@qq.com>
2022-08-04 21:44:49 +0800

    vmy reset Operators

--- src/main/java/com/silence/vmy/Operators.java
+++ src/main/java/com/silence/vmy/Operators.java
@@ -6,15 +6,22 @@ import java.util.Set;
 final public class Operators {
   private Operators(){}
   public static final Set<Character> buildinIdentifiers = new HashSet<>();
+
+  public static final Character ADD = '+';
+  public static final Character SUB = '-';
+  public static final Character MULTI = '*';
+  public static final Character DIVIDE = '/';
+  public static final Character OpenParenthesis = '(';
+  public static final Character ClosingParenthesis   = ')';
   
   static{
     // set buildinOperators
-    buildinIdentifiers.add('+');
-    buildinIdentifiers.add('-');
-    buildinIdentifiers.add('*');
-    buildinIdentifiers.add('/');
-    buildinIdentifiers.add('(');
-    buildinIdentifiers.add(')');
+    buildinIdentifiers.add(ADD);
+    buildinIdentifiers.add(SUB);
+    buildinIdentifiers.add(MULTI);
+    buildinIdentifiers.add(DIVIDE);
+    buildinIdentifiers.add(OpenParenthesis);
+    buildinIdentifiers.add(ClosingParenthesis);
   }
 
 
