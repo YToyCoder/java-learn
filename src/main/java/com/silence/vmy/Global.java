8eb143327dfa4c5b33efb35ab3f80c54f68af752
forsilence <3312034934@qq.com>
2022-08-11 14:18:41 +0800

    vmy
    Runtime - Global
    
    support global primitive values

--- src/main/java/com/silence/vmy/Global.java
+++ src/main/java/com/silence/vmy/Global.java
@@ -1,5 +1,26 @@
 package com.silence.vmy;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Global {
   private Global(){}
+
+  private static Global INSTANCE = new Global();
+
+  public static Global getInstance() {
+    return INSTANCE;
+  }
+
+  private Map<String, Object> primitives = new TreeMap<>();
+
+  public void put(String _name, Object _value){
+    if(_value instanceof Number || _value instanceof Boolean || _value instanceof Character){
+      primitives.put(_name, _value);
+    }
+  }
+
+  public Object get(String _name){
+    return primitives.get(_name);
+  }
 }
