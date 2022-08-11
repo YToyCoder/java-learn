a145caf45c588e879742cecb10529586b96508fb
forsilence <3312034934@qq.com>
2022-08-11 10:40:29 +0800

    vmy
    Identifiers support variable name / function name like :
    
     a , variable_name, kb

--- src/main/java/com/silence/vmy/Identifiers.java
+++ src/main/java/com/silence/vmy/Identifiers.java
@@ -1,14 +1,21 @@
 package com.silence.vmy;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.IntStream;
 
 final public class Identifiers {
   private Identifiers(){}
   public static final Set<String> builtinIdentifiers = new HashSet<>();
+  // support binary operation
   public static  final Set<Character> operatorCharacters = new HashSet<>();
+  // parenthesis : ( and )
   public static  final Set<Character> commonIdentifiers = new TreeSet<>();
+
+  // variable name / function name
+  public static  final Set<Character> identifiers = new TreeSet<>();
 
   public static final String ADD = "+";
   public static final String SUB = "-";
@@ -37,6 +44,16 @@ final public class Identifiers {
     commonIdentifiers.addAll(
         Set.of('(',')', '=')
     );
+    // Alphabetic 字母 a -> z & A -> Z
+    identifiers.addAll(
+        //
+        IntStream.range(0, 26)
+            .mapToObj(el -> List.of(Character.valueOf((char) (el + 'a')), Character.valueOf((char)(el + 'A'))))
+            .flatMap(ls -> ls.stream())
+            .toList()
+    );
+    // _ 下划线
+    identifiers.add('_');
   }
 
 
