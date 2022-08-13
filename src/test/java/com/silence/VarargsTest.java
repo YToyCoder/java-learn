eb9e6986455c7794f2d42b140a4b212739a1bcd3
forsilence <3312034934@qq.com>
2022-08-13 10:31:23 +0800

    java lang var args test

--- src/test/java/com/silence/VarargsTest.java
+++ src/test/java/com/silence/VarargsTest.java
@@ -10,10 +10,16 @@ public class VarargsTest {
       System.out.printf("args length is %d\n", args.length);
     }
   }
+
+  public void var_args_test_2(Object ...params){
+    System.out.println(params.length);
+  }
   
   @Test
   public void test() {
     testVarargs();
     testVarargs("1", "2");
+    testVarargs(new String[]{"123", "4567"});
+    var_args_test_2(new Object[]{1, 2});
   }
 }
