5b564ee1a4eb072108166a0fbb711b22c99d4a86
forsilence <3312034934@qq.com>
2022-08-13 10:32:53 +0800

    vmy - FunctionSupport , add new function tag (native) & doc

--- src/main/java/com/silence/vmy/FunctionSupport.java
+++ src/main/java/com/silence/vmy/FunctionSupport.java
@@ -15,13 +15,48 @@ public class FunctionSupport {
 
   public static final int Builtin = 0;
   public static final int UserDefined = 1;
+  public static final int Native = 2;
 
   public interface FunctionType extends VmyType{
+    /**
+     * @return types of all params
+     */
     List<VmyType> types();
+
+    /**
+     * a function's params can see as an array, if you want to get the param of 0 , it can be seen as params[0]
+     * @param i
+     * @return the type of param in location i, if 'i' is out of length for params return null
+     */
     VmyType param_type(int i);
+
+    /**
+     * @return Function tag , mark for user defined(user defined using vmy script) , builtin(builtin call) or Native(Write By Java)
+     *
+     * @see FunctionSupport#Builtin
+     * @see FunctionSupport#Native
+     * @see FunctionSupport#UserDefined
+     */
     int tag();
   }
 
+  /**
+   * register a function
+   */
   public static interface FunctionRegister {
+    void register(String name, FunctionType type, Callable callable);
+  }
+
+  /**
+   * create a function
+   */
+  public static interface FunctionFactory {
+    /**
+     * get a {@link Callable} by function name and function type
+     * @param name function name
+     * @param type function type
+     * @return {@link Callable} or {@code null}
+     */
+    Callable get_function(String name, FunctionType type);
   }
 }
