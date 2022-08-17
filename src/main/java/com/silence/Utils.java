5e443c65f7c1b6964646f3eb10066fd2d51bae40
forsilence <3312034934@qq.com>
2022-08-17 08:57:48 +0800

    util to print collection more readable

--- src/main/java/com/silence/Utils.java
+++ src/main/java/com/silence/Utils.java
@@ -1,9 +1,20 @@
 package com.silence;
 
+import java.util.Collection;
+
 public class Utils {
   public static final String project_dir = System.getProperty("user.dir");
 
   public static final String get_dir_of_project(String name){
     return String.format("%s/%s", project_dir, name);
+  }
+
+  public static <T> String collection_to_string(Collection<T> collection){
+    final StringBuilder builder = new StringBuilder("[");
+    for(T el : collection){
+      builder.append(el.toString()).append(" ");
+    }
+    builder.append("]");
+    return builder.toString();
   }
 }
