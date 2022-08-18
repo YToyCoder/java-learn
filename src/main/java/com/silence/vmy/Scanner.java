5bbe033e2b3d541e4687bd9fcdd359e6fe06ac6e
forsilence <3312034934@qq.com>
2022-08-18 17:48:33 +0800

    Scanner support register TokenHistoryHandler

--- src/main/java/com/silence/vmy/Scanner.java
+++ src/main/java/com/silence/vmy/Scanner.java
@@ -7,21 +7,32 @@ public interface Scanner {
   List<Token> scan(final String source);
 
   /**
-   * preview the next token
-   * @return
+   * preview the next token, do not remove it
+   * @return {@link Token}
    */
   Token peek();
 
   /**
-   * get the next token
-   * @return
+   * get the next token and remove it
+   * @return {@link Token}
    */
   Token next();
 
   /**
    * check if there is next token
-   * @return
+   * @return true if has token, else false
    */
   boolean hasNext();
+
+  /**
+   * register a {@link TokenHistoryRecorder} to record each used Token, 
+   * if want to this function full work,you need implement it by yourself.
+   * @param historyRecorder {@link TokenHistoryRecorder}
+   * @param force determine if replace the old TokenHistory when already exists a recorder, if true , then force replace it and return true, else if exists return false
+   * @return if set successful return true, else return false
+   */
+  default boolean register(TokenHistoryRecorder historyRecorder, boolean force){
+    return false;
+  }
 
 }
