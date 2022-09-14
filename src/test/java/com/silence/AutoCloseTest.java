ccb0e3fe09b19321909654a40ad9435041da5957
forsilence <3312034934@qq.com>
2022-09-14 10:43:03 +0800

    try - with - resources

--- src/test/java/com/silence/AutoCloseTest.java
+++ src/test/java/com/silence/AutoCloseTest.java
@@ -1,5 +1,7 @@
 package com.silence;
 
+import java.util.function.Supplier;
+
 import org.junit.Test;
 
 public class AutoCloseTest {
@@ -8,6 +10,46 @@ public class AutoCloseTest {
   public void test() {
     try (var obj = new AutoCloseableObj()){
     }catch(Exception e){
+    }
+  }
+
+  @Test
+  public void try_with_resources(){
+    try(TryClose close = new TryClose()){
+      close.throwEception();
+    }catch(Exception e){
+      System.out.println("exception in catch : " + e.getClass().getSimpleName());
+    }
+  }
+
+
+  @Test
+  public void try_with_resources_and_return(){
+    Supplier<String> s = () -> {
+      try(TryClose tryClose = new TryClose()){
+        tryClose.throwEception();
+        return "hello";
+      }catch(Exception e){
+        System.out.println("exception in catch : " + e.getClass().getSimpleName());
+        throw new RuntimeException(e);
+      }
+    };
+    try{
+      System.out.println(s.get()); 
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  static class TryClose implements AutoCloseable{
+
+    public void throwEception(){
+      throw new RuntimeException();
+    }
+
+    @Override
+    public void close() throws Exception {
+      System.out.println("closed");
     }
   }
 }
